aaf71ec513968f9f55d5f78383594065ad84060a
Marcin Małek <marcinmalek01@gmail.com>
2026-05-17 17:06:03 +0200

    linting and typechecking changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,19 +4,25 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import babel from '@rolldown/plugin-babel'
 
-// Nowoczesny sposób na zdobycie "bezwzględnej ścieżki" w ES Modules
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    watch: {
+      ignored: ['!**/node_modules/@pjm/shared/**']
+    }
+  },
   resolve: {
     alias: {
       '@mediapipe/hands': path.resolve(__dirname, './src/mock-mediapipe.ts')
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
   }
 })
