acdd84b56b6b789a18ade1689cfe6c95788696f1
Marcin Małek <marcinmalek01@gmail.com>
2026-05-25 23:33:53 +0200

    workaround for mediapipe quirks, changed hand pose hook to satisfy strict mode

--- frontend/src/hooks/useHandPose.tsx
+++ frontend/src/hooks/useHandPose.tsx
@@ -1,27 +1,28 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
 import '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-webgl';
 import { fingerJoints } from '@pjm/shared/consts';
 
-import type { MediaPipeHandsTfjsModelConfig, HandDetector, Hand } from '@tensorflow-models/hand-pose-detection';
+import type { HandDetector, Hand } from '@tensorflow-models/hand-pose-detection';
+
+let detector: HandDetector | null = import.meta.hot?.data?.detector || null;
+let isInitializing = import.meta.hot?.data?.isInitializing || false;
+
+if (import.meta.hot) {
+  import.meta.hot.dispose((data) => {
+    data.detector = detector;
+    data.isInitializing = isInitializing;
+  });
+}
 
 export const useHandPose = (onDetection: (hands: Hand[]) => void) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [detector, setDetector] = useState<HandDetector | null>(null);
 
   useEffect(() => {
+    let animationId: number;
     const init = async () => {
-      const model = handPoseDetection.SupportedModels.MediaPipeHands;
-      const detectorConfig: MediaPipeHandsTfjsModelConfig = {
-        runtime: 'tfjs',
-        modelType: 'full',
-        maxHands: 2,
-      };
-      const net = await handPoseDetection.createDetector(model, detectorConfig);
-      setDetector(net);
-
       const stream = await navigator.mediaDevices.getUserMedia({
         video: { width: 640, height: 480, frameRate: 30 },
       });
@@ -30,12 +31,36 @@ export const useHandPose = (onDetection: (hands: Hand[]) => void) => {
         videoRef.current.width = 640;
         videoRef.current.height = 480;
       }
+      if (detector) {
+        detect();
+        return;
+      }
+      if (isInitializing) {
+        return;
+      }
+      isInitializing = true;
+      try {
+        detector = await handPoseDetection.createDetector(
+          handPoseDetection.SupportedModels.MediaPipeHands,
+          {
+            runtime: 'mediapipe',
+            solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands/',
+            modelType: 'lite',
+            maxHands: 2,
+          },
+          // {
+          //   runtime: 'tfjs',
+          //   modelType: 'full',
+          //   maxHands: 2,
+          // }
+        );
+        detect();
+      } catch (error) {
+        console.error('Error initializing hand pose detection:', error);
+      } finally {
+        isInitializing = false;
+      }
     };
-    init();
-  }, []);
-
-  useEffect(() => {
-    let animationId: number;
     const detect = async () => {
       if (detector && videoRef.current && canvasRef.current && videoRef.current.readyState === 4) {
         const hands = await detector.estimateHands(videoRef.current, { flipHorizontal: true });
@@ -74,9 +99,10 @@ export const useHandPose = (onDetection: (hands: Hand[]) => void) => {
       }
       animationId = requestAnimationFrame(detect);
     };
-    detect();
+
+    init();
     return () => cancelAnimationFrame(animationId);
-  }, [detector, onDetection]);
+  }, [onDetection]);
 
   return { videoRef, canvasRef };
 };
--- frontend/src/mock-mediapipe.ts
+++ frontend/src/mock-mediapipe.ts
@@ -1,2 +1,3 @@
-export class Hands {}
+// @ts-expect-error - This file is a mock for the @mediapipe/hands library, which is loaded via a script tag in index.html.
+export const Hands = window.Hands;
 export const VERSION = '0.0.0';
